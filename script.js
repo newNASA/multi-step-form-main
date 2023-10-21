605262df3972058507ef656c181a2e02fc45d039
newNASA <azizbekgulomovweb@gmail>
2023-10-21 15:28:44 +0500

    commit

--- script.js
+++ script.js
@@ -40,6 +40,10 @@ const choices_three = document.querySelector('.three');
 choices_one.style.backgroundColor = "var(--magnolia)";
 choices_one.style.borderColor = "var(--button-hover)";
 
+let check_choice_one = true;
+let check_choice_two = false;
+let check_choice_three = false;
+
 choices_one.addEventListener('click', () => {
     
     choices_one.style.backgroundColor = "var(--magnolia)";
@@ -49,6 +53,10 @@ choices_one.addEventListener('click', () => {
     choices_two.style.borderColor = "";
     choices_three.style.backgroundColor = "";
     choices_three.style.borderColor = "";
+    
+    check_choice_one = true;
+    check_choice_three = false;
+    check_choice_two = false;
     
 })
 
@@ -61,6 +69,10 @@ choices_two.addEventListener('click', () => {
     choices_one.style.borderColor = "";
     choices_three.style.backgroundColor = "";
     choices_three.style.borderColor = "";
+
+    check_choice_one = false;
+    check_choice_two = true;
+    check_choice_three = false;
     
 })
 
@@ -73,6 +85,10 @@ choices_three.addEventListener('click', () => {
     choices_one.style.borderColor = "";
     choices_two.style.backgroundColor = "";
     choices_two.style.borderColor = "";
+    
+    check_choice_one = false;
+    check_choice_two = false;
+    check_choice_three = true;
     
 })
 
@@ -92,39 +108,9 @@ const select__plan = document.querySelector(".select__plan");
 const add__ons__page = document.querySelector(".add__ons__page");
 const step__three_div = document.querySelector(".step__three_div");
 
-let counter = 1;
-monthly.style.color = "var(--title-color)";
 
-billing.addEventListener('click', () => {
-    if (counter % 2 == 1) {
-        billing.style.paddingLeft = "28px";
-        monthly.style.color = "";
-        yearly.style.color = "var(--title-color)";
-        arcade_price.textContent = "$90/yr";
-        advanced_price.textContent = "$120/yr";
-        pro_price.textContent = "$150/yr";
-        checkout.forEach(element => {
-            element.style.display = "block";
-        });
-        online_service_price.textContent = "+$10/yr";
-        larger_storage_price.textContent = "+$20/yr";
-        custom_pro_price.textContent = "+$20/yr";
-    } else {
-        billing.style.paddingLeft = ""; 
-        monthly.style.color = "var(--title-color)";
-        yearly.style.color = "";
-        arcade_price.textContent = "$9/mo";
-        advanced_price.textContent = "$12/mo";
-        pro_price.textContent = "$15/mo";
-        checkout.forEach(element => {
-            element.style.display = "";
-        });
-        online_service_price.textContent = "+$1/mo";
-        larger_storage_price.textContent = "+$2/mo";
-        custom_pro_price.textContent = "+$2/mo";
-    }
-    counter++;
-});
+
+
 
 
 go_back__to__your_info.addEventListener("click", () => {
@@ -226,6 +212,9 @@ const go_back__to__add_ons = document.querySelector(".go__add_ons");
 const add__ons = document.querySelector(".add__ons");
 const step__four_div = document.querySelector(".step__four_div");
 
+const rate_name = document.querySelector(".rate_name");
+const rate_price = document.querySelector(".rate_price");
+
 add__ons.addEventListener("click", function (){
     add__ons__page.style.display = "none";
     summary__page.style.display = "block";
@@ -242,3 +231,64 @@ go_back__to__add_ons.addEventListener("click", function () {
     step__three_div.style.background = "var(--light-blue)";
     step__three_div.children[0].style.color = "var(--button-hover)";
 })
+
+var billing_checker = true;
+let counter = 1;
+monthly.style.color = "var(--title-color)";
+
+billing.addEventListener('click', () => {
+    if (counter % 2 == 1) {
+        billing.style.paddingLeft = "28px";
+        monthly.style.color = "";
+        yearly.style.color = "var(--title-color)";
+        arcade_price.textContent = "$90/yr";
+        advanced_price.textContent = "$120/yr";
+        pro_price.textContent = "$150/yr";
+        billing_checker = false;
+        checkout.forEach(element => {
+            element.style.display = "block";
+        });
+        online_service_price.textContent = "+$10/yr";
+        larger_storage_price.textContent = "+$20/yr";
+        custom_pro_price.textContent = "+$20/yr";
+    } else {
+        billing.style.paddingLeft = ""; 
+        monthly.style.color = "var(--title-color)";
+        yearly.style.color = "";
+        arcade_price.textContent = "$9/mo";
+        advanced_price.textContent = "$12/mo";
+        pro_price.textContent = "$15/mo";
+        checkout.forEach(element => {
+            element.style.display = "";
+        });
+        online_service_price.textContent = "+$1/mo";
+        larger_storage_price.textContent = "+$2/mo";
+        custom_pro_price.textContent = "+$2/mo";
+        billing_checker = true;
+    }
+    counter += 1; 
+});
+
+if (billing_checker == true) {
+    if (check_choice_one){
+        rate_name.textContent = "Arcade (monthly)";
+        rate_price.textContent = "$9/mo";
+    }else if (check_choice_two){
+        rate_name.textContent = "Advanced (monthly)";
+        rate_price.textContent = "$12/mo"
+    }else if(check_choice_three){
+        rate_name.textContent = "Pro (monthly)"
+        rate_price.textContent = "$15/mo"
+    }
+}else{
+    if (check_choice_one){
+        rate_name.textContent = "Arcade (yearly)";
+        rate_price.textContent = "$90/yr";
+    }else if (check_choice_two){
+        rate_name.textContent = "Advanced (yearly)";
+        rate_price.textContent = "$120/yr"
+    }else if (check_choice_three){
+        rate_name.textContent = "Pro (yearly)"
+        rate_price.textContent = "$150/yr"
+    }
+}
